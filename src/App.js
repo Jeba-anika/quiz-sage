c3c5d18b4e1af6bd2c1fa5ac9a35539ec3fe9c16
Jeba-anika <anikanur98@gmail.com>
2022-10-12 19:50:51 +0600

    topic page edited

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ function App() {
         },
         {
           path: '/topics',
+          loader: async()=>{
+            return fetch('https://openapi.programming-hero.com/api/quiz')
+          },
           element: <Topic></Topic>
         },
         {
--- src/Component/Topic/Topic.js
+++ src/Component/Topic/Topic.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import './Topic.css'
 
 const Topic = () => {
+    const data  = useLoaderData().data;
+    console.log(data)
+    
     return (
-        <div>
-            <h2>Topics</h2>
+        <div className='container mt-5 topic'>
+            <div className='row row-cols-lg-4 row-cols-md-4 '>
+            {
+                data.map(topic => <div>
+                    
+                    <img className='topic-img' src={topic.logo} alt="" />
+                    <p className='fw-bold'>{topic.name}</p>
+                    
+                </div>)
+            }
+            </div>
         </div>
     );
 };
